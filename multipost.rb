ece1f15ae629974e72d36c47c14f36f39563d342
Scott Clark <scottclark11@gmail.com>
2016-02-12 00:29:26 -0500

    hacky 1st iteration for adding jekyll 3 support and addressig #2

--- multipost.rb
+++ multipost.rb
@@ -1,47 +1,77 @@
 require 'cgi'
 
+# TODO: This is a very bad hacky-but-working first iteration. Refactoring needed:
+#   * Break stuff out into separate classes/modules wherever it makes sense
+#   * D.R.Y. up the code
+#   * Don't access private members
+
 module Jekyll
   class MultiPostGenerator < Generator
     safe true
 
     def generate(site)
-      generate_views(site, :posts)
-      generate_views(site, :pages)
+      generate_page_layouts(site)
+      generate_collection_item_layouts(site)
     end
 
     private
 
-    def generate_views(site, resource_type)
-      klass = resource_klass(resource_type)
+    #  PAGES
+    # ==================================================================
 
-      site.send(resource_type).map! do |resource|
-        resource.data["layout"].is_a?(Array) ? generate_layouts(resource, klass) : resource
+    def generate_page_layouts(site)
+      site.pages.map! do |page|
+        if page.data["layout"].is_a?(Array)
+          create_page_layout_views(page)
+        else
+          page
+        end
       end.flatten!
     end
 
-    def resource_klass(resource_type)
-      klasses = {
-        :posts => Post,
-        :pages => Page
-      }
-
-      klasses[resource_type]
-    end
-
-    def generate_layouts(resource, klass)
-      resource.data["layout"].map do |layout|
-        view = klass.new(resource.site, resource.site.source, resource_dir(resource), resource.name)
-        view.data["layout"] = layout
-        view.data["permalink"] = view_permalink(resource, layout)
-        view
+    def create_page_layout_views(page)
+      page.data["layout"].map do |layout|
+        # TODO: dir is the string path between jekyll site repo root and page source file
+        # Figure out a way to get dir without accessing the page's private `@dir` getter. That's just bad.
+        dir = get_page_dir(page)
+        new_page = Page.new(page.site, page.site.source, dir, page.name)
+        new_page.data["layout"] = layout
+        new_page.data["permalink"] = get_adjusted_permalink(page, layout)
+        new_page
       end
     end
 
-    def resource_dir(resource)
-      resource.instance_variable_get(:@dir)
+    #  Collections
+    # ==================================================================
+
+    def generate_collection_item_layouts(site)
+      site.collections.each do |_, collection|
+        collection.docs.map! do |document|
+          if document.data["layout"].is_a?(Array)
+            create_document_layout_views(site, collection, document)
+          else
+            document
+          end
+        end.flatten!
+      end
     end
 
-    def view_permalink(resource, layout)
+    def create_document_layout_views(site, collection, document)
+      document.data["layout"].map do |layout|
+        doc = Document.new(document.path, {
+          :site => site,
+          :collection => collection
+        })
+        doc.data["layout"] = layout
+        doc.data["permalink"] = get_adjusted_permalink(document, layout)
+        doc
+      end
+    end
+
+    #  Helpers
+    # ==================================================================
+
+    def get_adjusted_permalink(resource, layout)
       layout_path = CGI.escape(layout)
       url = resource.url
       ext = File.extname(url)
@@ -55,6 +85,11 @@ module Jekyll
       else
         url.gsub(/\/$|#{ext}$/) { |url_end| "/#{layout_path}#{url_end}" }
       end
+    end
+
+    def get_page_dir(page)
+      # Don't do this. This is awful. Don't ever do this. Why am I doing this?
+      page.instance_variable_get(:@dir)
     end
   end
 end
